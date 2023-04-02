28cc68d7a25f21a1e07cc6d320503f7cddcb8458
Shadae <shadaejadeellmers@gmail.com>
2023-04-02 12:47:26 +1200

    route complete for get all

---
+++ server/routes/read.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import { getAllBooks } from '../db/readdb'
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  getAllBooks()
+    .then((books) => {
+      res.json(books)
+    })
+    .catch((err: Error) => {
+      res.status(500).send(err.message)
+    })
+})
