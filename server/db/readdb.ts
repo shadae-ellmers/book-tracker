c905e8310ee48abb58c4163797944242a8e60739
Shadae <shadaejadeellmers@gmail.com>
2023-04-02 19:41:05 +1200

    db complete for adding book

--- server/db/readdb.ts
+++ server/db/readdb.ts
@@ -6,3 +6,17 @@ import { Read, ReadInfo } from '../../common/Read'
 export function getAllBooks(db = connection): Promise<Read[]> {
   return db('read').select('*')
 }
+
+// get a book
+
+export function getBook(id: number, db = connection): Promise<Read> {
+  return db('read').first().where({ id })
+}
+
+// add book
+
+export function addBook(book: ReadInfo, db = connection): Promise<Read> {
+  return db('read')
+    .insert(book)
+    .then(([id]) => ({ id, ...book }))
+}
