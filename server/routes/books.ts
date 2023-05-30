740a8dd5db0c4163b4d539dce377ab7cfa8329e5
Shadae <shadaejadeellmers@gmail.com>
2023-05-30 20:14:09 +1200

    routes complete for to-read

--- server/routes/books.ts
+++ server/routes/books.ts
@@ -1,7 +1,9 @@
 import express from 'express'
-import { getAllBooks, addBook, addRating } from '../db/db'
+import { getAllBooks, addBook, addRating, getAllToReadBooks } from '../db/db'
 
 const router = express.Router()
+
+// read books routes
 
 router.get('/', (req, res) => {
   getAllBooks()
@@ -29,6 +31,29 @@ router.patch('/addrating/', (req, res) => {
   addRating(book)
     .then((book) => {
       res.json(book)
+    })
+    .catch((err: Error) => {
+      res.status(500).send(err.message)
+    })
+})
+
+// to-read books routes
+
+router.get('/toread', (req, res) => {
+  getAllToReadBooks()
+    .then((books) => {
+      res.json(books)
+    })
+    .catch((err: Error) => {
+      res.status(500).send(err.message)
+    })
+})
+
+router.post('/toread', (req, res) => {
+  const book = req.body
+  addBook(book)
+    .then((bookArray) => {
+      res.json(bookArray[0])
     })
     .catch((err: Error) => {
       res.status(500).send(err.message)
