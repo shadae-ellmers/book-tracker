63bfb30dfc0a8bdfa7571e22c8955de4c7336194
Shadae <shadaejadeellmers@gmail.com>
2023-06-05 18:32:38 +1200

    actions set up for finish reading book

--- client/actions/read.ts
+++ client/actions/read.ts
@@ -1,10 +1,11 @@
 import type { ThunkAction } from '../store'
-import { Book } from '../../common/Book'
-import { getBooks, addARating, deleteARead } from '../apis/readApi'
+import { Book, BookInfo } from '../../common/Book'
+import { getBooks, addARating, deleteARead, postOneBook } from '../apis/readApi'
 import { setError } from './error'
 
 export type BookAction =
   | { type: 'SET_READ'; payload: Book[] }
+  | { type: 'SAVE_ONE_READ'; payload: Book }
   | { type: 'ADD_RATING'; payload: Book }
   | { type: 'DEL_READ'; payload: number }
 
@@ -12,6 +13,13 @@ export function showRead(setRead: Book[]): BookAction {
   return {
     type: 'SET_READ',
     payload: setRead,
+  }
+}
+
+export function saveOneRead(book: Book): BookAction {
+  return {
+    type: 'SAVE_ONE_READ',
+    payload: book,
   }
 }
 
@@ -58,6 +66,18 @@ export function delReadThunk(id: number): ThunkAction {
     return deleteARead(id)
       .then(() => {
         dispatch(delRead(id))
+      })
+      .catch((err) => {
+        dispatch(setError(err.message))
+      })
+  }
+}
+
+export function addToCurrent(book: BookInfo): ThunkAction {
+  return (dispatch) => {
+    return postOneBook(book)
+      .then((book) => {
+        dispatch(saveOneRead(book))
       })
       .catch((err) => {
         dispatch(setError(err.message))
