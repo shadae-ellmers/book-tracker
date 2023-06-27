098a735853f9dc3e48cd6e493f2f29fe7413d83a
Shadae <shadaejadeellmers@gmail.com>
2023-06-27 21:05:32 +1200

    condensing for less code

--- client/components/Home.tsx
+++ client/components/Home.tsx
@@ -28,9 +28,9 @@ export default function Home() {
     <section className="text-2xl leading-loose">
       <p className="underline text-3xl font-extrabold">Currently Reading</p>
       {/* map not working on deploy */}
-      {currentread ? (
-        <div>
-          {currentread.map((book) => (
+      <div>
+        {currentread &&
+          currentread.map((book) => (
             <div key={book.id} className="flex flex-row py-3">
               <img src={book.cover} alt="book cover" className="block pr-5" />
               <div className="flex flex-col justify-evenly">
@@ -71,10 +71,7 @@ export default function Home() {
               </div>
             </div>
           ))}
-        </div>
-      ) : (
-        <></>
-      )}
+      </div>
     </section>
   )
 }
