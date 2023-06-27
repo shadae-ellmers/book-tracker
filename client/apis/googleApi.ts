bfb32d580d7f4a7765dafc16079135bde88a74a1
Shadae <shadaejadeellmers@gmail.com>
2023-06-27 13:14:56 +1200

    attempt to fix deploy 4

--- client/apis/googleApi.ts
+++ client/apis/googleApi.ts
@@ -6,6 +6,8 @@ const googleUrl = 'https://www.googleapis.com/books/v1/volumes?q='
 export function searchForBook(book: string): Promise<GoogleBook[]> {
   return request
     .get(`${googleUrl}${book}+&key=${process.env.GOOGLE_KEY}&maxResults=5`)
+    .set('Content-Type', 'application/json')
+    .set('Authorization', `Bearer ${process.env.GOOGLE_KEY}`)
     .then((res) => {
       return res.body
     })
