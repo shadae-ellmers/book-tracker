252074f55efa94f504af1e5e633f53c6d13e55e5
Shadae <shadaejadeellmers@gmail.com>
2023-06-27 17:10:52 +1200

    attempt to fix deploy 5

--- client/apis/googleApi.ts
+++ client/apis/googleApi.ts
@@ -4,11 +4,14 @@ import { GoogleBook } from '../../common/Book'
 const googleUrl = 'https://www.googleapis.com/books/v1/volumes?q='
 
 export function searchForBook(book: string): Promise<GoogleBook[]> {
-  return request
-    .get(`${googleUrl}${book}+&key=${process.env.GOOGLE_KEY}&maxResults=5`)
-    .set('Content-Type', 'application/json')
-    .set('Authorization', `Bearer ${process.env.GOOGLE_KEY}`)
-    .then((res) => {
-      return res.body
-    })
+  return (
+    request
+      .get(`${googleUrl}${book}+&key=${process.env.GOOGLE_KEY}&maxResults=5`)
+      .set('Content-Type', 'application/json')
+      // .set('Authorization', `Bearer ${process.env.GOOGLE_KEY}`)
+      // .set('Authorization', `Bearer ${process.env.VERCEL_KEY}`)
+      .then((res) => {
+        return res.body
+      })
+  )
 }
